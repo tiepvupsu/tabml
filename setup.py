58d0156268aed2d70ae31ce987be4934ce2f8baf
Tiep Vu <vuhuutiep@gmail.com>
2021-03-19 00:15:26 -0700

    📦 NEW: add setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import find_packages, setup
+
+with open("README.md") as f:
+    readme = f.read()
+
+with open("LICENSE") as f:
+    license = f.read()
+
+setup(
+    name="tabml",
+    version="0.1",
+    description="Machine Learning with Tabular data",
+    long_description=readme,
+    author="Tiep Vu",
+    author_email="vuhuutiep@gmail.com",
+    url="https://github.com/tiepvupsu/tabml",
+    license=license,
+    packages=find_packages(exclude=("tests", "docs")),
+)
