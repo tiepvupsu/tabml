a42d0510b5f112225c2c5ff18d071409e283063d
Tiep Vu <vuhuutiep@gmail.com>
2021-03-19 15:11:41 -0700

    add pb_helpers.py

---
+++ tabml/utils/pb_helpers.py
@@ -0,0 +1,12 @@
+from typing import Union
+
+from google.protobuf import text_format
+
+from tabml.protos import feature_manager_pb2
+
+
+def parse_feature_config_pb(feature_manager_pbtxt_path: str):
+    with open(feature_manager_pbtxt_path, "r") as file_object:
+        config = feature_manager_pb2.FeatureManager()
+        pb = text_format.MergeLines(file_object, config)
+    return pb
