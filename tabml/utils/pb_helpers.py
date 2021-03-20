1af38897416efe39ca7bb26968b050ddbd298951
Tiep Vu <vuhuutiep@gmail.com>
2021-03-20 13:21:49 -0700

    👌 IMPROVE: rename FeatureManger to FeatureConfig

--- tabml/utils/pb_helpers.py
+++ tabml/utils/pb_helpers.py
@@ -5,6 +5,6 @@ from tabml.protos import feature_manager_pb2
 
 def parse_feature_config_pb(feature_manager_pbtxt_path: str):
     with open(feature_manager_pbtxt_path, "r") as file_object:
-        config = feature_manager_pb2.FeatureManager()
+        config = feature_manager_pb2.FeatureConfig()
         pb = text_format.MergeLines(file_object, config)
     return pb
