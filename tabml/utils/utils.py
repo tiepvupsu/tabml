7e0d31e430aa1912197eb54c85182675a40ecaaa
Tiep Vu <vuhuutiep@gmail.com>
2021-03-20 13:41:39 -0700

    :truck: move _check_uniqueness to utils.utils

--- tabml/utils/utils.py
+++ tabml/utils/utils.py
@@ -1,5 +1,6 @@
 import random
 import string
+from typing import Any, Collection, Set
 
 
 def random_string(length: int = 10) -> str:
@@ -20,3 +21,25 @@ def write_str_to_file(a_str: str, filename: str) -> None:
     """Writes a string to a file."""
     with open(filename, "w") as str_file:
         str_file.write(a_str)
+
+
+def check_uniqueness(items: Collection) -> None:
+    """Checks if an array containing unique elements.
+
+    Args:
+        items: A list of objects.
+
+    Returns:
+        Does not return anything. If this function passes, it means that all objects
+        are unique.
+
+    Raises:
+        Assertion error with list of duplicate objects.
+    """
+    seen_items: Set[Any] = set()
+    duplicates = set()
+    for item in items:
+        if item in seen_items:
+            duplicates.add(item)
+        seen_items.add(item)
+    assert not duplicates, f"There are duplicate objects in the list: {duplicates}."
