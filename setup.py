b235e6850edd4f3f68429bf74afaba24eb860f1f
Tiep Vu <vuhuutiep@gmail.com>
2021-03-28 11:22:32 -0700

    add install_requires into setup.py

--- setup.py
+++ setup.py
@@ -16,4 +16,16 @@ setup(
     url="https://github.com/tiepvupsu/tabml",
     license=license,
     packages=find_packages(exclude=("tests", "docs")),
+    install_requires=[
+        "GitPython==3.1.13",
+        "kaggle==1.5.6",
+        "lightgbm==2.3.1",
+        "loguru==0.5.1",
+        "numpy==1.18.5",
+        "pandas==1.1.4",
+        "pandas-profiling==2.9.0",
+        "protobuf==3.13.0",
+        "scikit-learn==0.24.1",
+        "scipy==1.4.1",
+    ],
 )
