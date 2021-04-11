71c092241cce73494e9d49f47e8a2be1cb9921e7
Tiep Vu <vuhuutiep@gmail.com>
2021-04-11 16:19:11 -0700

    rewrite data_processing.transform -> fit_train_transform_all

--- tabml/data_processing.py
+++ tabml/data_processing.py
@@ -1,4 +1,4 @@
-from typing import Optional, Tuple
+from typing import List, Optional, Tuple
 
 import pandas as pd
 from sklearn.base import BaseEstimator, TransformerMixin
@@ -76,8 +76,29 @@ class BoxplotOutlierClipper(BaseEstimator, TransformerMixin):
         return X.clip(self.lower, self.upper, axis=0)
 
 
-def transform(df, columns, training_filters, transformer):
-    all_data = df[columns]
-    train_data = df.query(" and ".join(training_filters))[columns]
+def fit_train_transform_all(
+    whole_df: pd.DataFrame, columns: List[str], training_filters: List[str], transformer
+):
+    """Fits transformer on training data and use it to transform all data.
+
+    This is essentially helpful when we want to apply feature transformer only to
+    training data to avoid data leakage.
+
+    Args:
+        whole_df:
+            The whole dataframe, including all data and neccessary columns for filtering
+            training data.
+        columns:
+            A list of column names used to fit the transformer
+        training_filters:
+            A list of filters used to extract training data
+        transformer:
+            A sklearn-like transformer which has .fit() and transform() methods.
+
+    Returns:
+        Transformed array of all data.
+    """
+    all_data = whole_df[columns]
+    train_data = whole_df.query(" and ".join(training_filters))[columns]
     transformer.fit(X=train_data)
     return transformer.transform(all_data)
