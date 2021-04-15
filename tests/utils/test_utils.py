94bccd8b2954017a57c70d96a78107ad80929b5b
Tiep Vu <vuhuutiep@gmail.com>
2021-04-14 20:33:45 -0700

    add test for show_feature_importance

--- tests/utils/test_utils.py
+++ tests/utils/test_utils.py
@@ -1,6 +1,6 @@
 import random
 
-from qcore.asserts import assert_eq
+from qcore.asserts import AssertRaises, assert_eq
 
 from tabml.utils import utils
 
@@ -18,3 +18,22 @@ class TestRandomString:
         # check two calls return differnt strings.
         # There is an extremely low chance that these two random strings are equal
         assert_eq(2, len(set([str1, str2])))
+
+
+class TestShowFeatureImportance:
+    def test_1(self):
+        data = {"a": 1, "b": 10, "c": 5}
+        utils.show_feature_importance(data)
+
+    def test_negative_importance(self):
+        data = {"a": -1, "b": 0, "c": 1}
+        with AssertRaises(ValueError) as assert_raises:
+            _ = utils.show_feature_importance(data)
+
+        assertion_error = assert_raises.expected_exception_found
+        assert_eq(
+            True,
+            assertion_error.args[0].startswith(
+                "All feature importances need to be non-negative, got data = "
+            ),
+        )
