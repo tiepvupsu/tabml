15d550624fed3ba5b6a81d5d5e11e532de0d0de6
Tiep Vu <vuhuutiep@gmail.com>
2021-04-16 16:30:13 -0700

    add analyize_model to housing pipeline

--- tabml/housing/pipelines.py
+++ tabml/housing/pipelines.py
@@ -5,6 +5,7 @@ def train_lgbm():
     path_to_config = "tabml/housing/configs/lgbm_config.pb"
     pipeline = BasePipeline(path_to_config)
     pipeline.run()
+    pipeline.analyze_model()
 
 
 if __name__ == "__main__":
