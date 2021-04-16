28c0d8aae748154cdcd2a8fdf0cdf8452e96f27e
Tiep Vu <vuhuutiep@gmail.com>
2021-04-15 18:46:08 -0700

    add parse_pipeline_config_pb to pb_helpers

--- tabml/utils/pb_helpers.py
+++ tabml/utils/pb_helpers.py
@@ -1,10 +1,15 @@
 from google.protobuf import text_format
 
-from tabml.protos import feature_manager_pb2
+from tabml.protos import feature_manager_pb2, pipeline_pb2
 
 
 def parse_feature_config_pb(feature_manager_pbtxt_path: str):
     with open(feature_manager_pbtxt_path, "r") as file_object:
         config = feature_manager_pb2.FeatureConfig()
-        pb = text_format.MergeLines(file_object, config)
-    return pb
+        return text_format.MergeLines(file_object, config)
+
+
+def parse_pipeline_config_pb(pipeline_pbtxt_path: str):
+    with open(pipeline_pbtxt_path, "r") as file_object:
+        config = pipeline_pb2.Config()
+        return text_format.MergeLines(file_object, config)
