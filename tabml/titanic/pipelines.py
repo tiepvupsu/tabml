be54196981b6c09f2351c9b1dfd16c7a0dcd403a
Tiep Vu <vuhuutiep@gmail.com>
2021-04-17 00:09:50 -0700

    add titanic/pipelines.py

---
+++ tabml/titanic/pipelines.py
@@ -0,0 +1,12 @@
+from tabml.pipelines import BasePipeline
+
+
+def train_lgbm():
+    path_to_config = "tabml/titanic/configs/lgbm_config.pb"
+    pipeline = BasePipeline(path_to_config)
+    pipeline.run()
+    pipeline.analyze_model()
+
+
+if __name__ == "__main__":
+    train_lgbm()
