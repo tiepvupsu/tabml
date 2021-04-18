31ec56ed0491f6af2d59756dfa3f9e0a4fa8963c
Tiep Vu <vuhuutiep@gmail.com>
2021-04-17 17:21:56 -0700

    pipeline proto: remove metrics, it's moved to trainer params

--- tabml/protos/pipeline_pb2.py
+++ tabml/protos/pipeline_pb2.py
@@ -22,7 +22,7 @@ DESCRIPTOR = _descriptor.FileDescriptor(
   syntax='proto2',
   serialized_options=None,
   create_key=_descriptor._internal_create_key,
-  serialized_pb=b'\n\x1btabml/protos/pipeline.proto\x12\x0ctabml.protos\x1a!tabml/protos/model_wrappers.proto\x1a\x1btabml/protos/trainers.proto\x1a\x1ftabml/protos/data_loaders.proto\"5\n\rModelAnalysis\x12\x0f\n\x07metrics\x18\x01 \x03(\t\x12\x13\n\x0b\x62y_features\x18\x02 \x03(\t\" \n\x05Saver\x12\x17\n\x0fsubmission_name\x18\x01 \x01(\t\"\x91\x02\n\x06\x43onfig\x12\x13\n\x0b\x63onfig_name\x18\x07 \x02(\t\x12-\n\x0b\x64\x61ta_loader\x18\x01 \x02(\x0b\x32\x18.tabml.protos.DataLoader\x12\x31\n\rmodel_wrapper\x18\x02 \x02(\x0b\x32\x1a.tabml.protos.ModelWrapper\x12&\n\x07trainer\x18\x03 \x02(\x0b\x32\x15.tabml.protos.Trainer\x12\x0f\n\x07metrics\x18\x04 \x03(\t\x12\x33\n\x0emodel_analysis\x18\x08 \x01(\x0b\x32\x1b.tabml.protos.ModelAnalysis\x12\"\n\x05saver\x18\x05 \x01(\x0b\x32\x13.tabml.protos.Saver'
+  serialized_pb=b'\n\x1btabml/protos/pipeline.proto\x12\x0ctabml.protos\x1a!tabml/protos/model_wrappers.proto\x1a\x1btabml/protos/trainers.proto\x1a\x1ftabml/protos/data_loaders.proto\"5\n\rModelAnalysis\x12\x0f\n\x07metrics\x18\x01 \x03(\t\x12\x13\n\x0b\x62y_features\x18\x02 \x03(\t\" \n\x05Saver\x12\x17\n\x0fsubmission_name\x18\x01 \x01(\t\"\x80\x02\n\x06\x43onfig\x12\x13\n\x0b\x63onfig_name\x18\x07 \x02(\t\x12-\n\x0b\x64\x61ta_loader\x18\x01 \x02(\x0b\x32\x18.tabml.protos.DataLoader\x12\x31\n\rmodel_wrapper\x18\x02 \x02(\x0b\x32\x1a.tabml.protos.ModelWrapper\x12&\n\x07trainer\x18\x03 \x02(\x0b\x32\x15.tabml.protos.Trainer\x12\x33\n\x0emodel_analysis\x18\x08 \x01(\x0b\x32\x1b.tabml.protos.ModelAnalysis\x12\"\n\x05saver\x18\x05 \x01(\x0b\x32\x13.tabml.protos.Saver'
   ,
   dependencies=[tabml_dot_protos_dot_model__wrappers__pb2.DESCRIPTOR,tabml_dot_protos_dot_trainers__pb2.DESCRIPTOR,tabml_dot_protos_dot_data__loaders__pb2.DESCRIPTOR,])
 
@@ -137,21 +137,14 @@ _CONFIG = _descriptor.Descriptor(
       is_extension=False, extension_scope=None,
       serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
     _descriptor.FieldDescriptor(
-      name='metrics', full_name='tabml.protos.Config.metrics', index=4,
-      number=4, type=9, cpp_type=9, label=3,
-      has_default_value=False, default_value=[],
-      message_type=None, enum_type=None, containing_type=None,
-      is_extension=False, extension_scope=None,
-      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
-    _descriptor.FieldDescriptor(
-      name='model_analysis', full_name='tabml.protos.Config.model_analysis', index=5,
+      name='model_analysis', full_name='tabml.protos.Config.model_analysis', index=4,
       number=8, type=11, cpp_type=10, label=1,
       has_default_value=False, default_value=None,
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
     _descriptor.FieldDescriptor(
-      name='saver', full_name='tabml.protos.Config.saver', index=6,
+      name='saver', full_name='tabml.protos.Config.saver', index=5,
       number=5, type=11, cpp_type=10, label=1,
       has_default_value=False, default_value=None,
       message_type=None, enum_type=None, containing_type=None,
@@ -170,7 +163,7 @@ _CONFIG = _descriptor.Descriptor(
   oneofs=[
   ],
   serialized_start=232,
-  serialized_end=505,
+  serialized_end=488,
 )
 
 _CONFIG.fields_by_name['data_loader'].message_type = tabml_dot_protos_dot_data__loaders__pb2._DATALOADER
