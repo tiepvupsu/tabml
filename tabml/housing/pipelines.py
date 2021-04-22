51c9588fb3d2e78747bca4bbf3f9e608c4ed7dbd
Tiep Vu <vuhuutiep@gmail.com>
2021-04-21 19:46:30 -0700

    add train_tabnet to housing pipeliens.py

--- tabml/housing/pipelines.py
+++ tabml/housing/pipelines.py
@@ -8,5 +8,13 @@ def train_lgbm():
     pipeline.analyze_model()
 
 
+def train_tabnet():
+    path_to_config = "tabml/housing/configs/tabnet_config.pb"
+    pipeline = BasePipeline(path_to_config)
+    pipeline.run()
+    pipeline.analyze_model()
+
+
 if __name__ == "__main__":
-    train_lgbm()
+    # train_lgbm()
+    train_tabnet()
