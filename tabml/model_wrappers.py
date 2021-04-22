e6755f0e384eeaea6b42c6f27ad6a361c7eb5ec5
Tiep Vu <vuhuutiep@gmail.com>
2021-04-21 19:08:50 -0700

    add BaseTabNetModelWraper

--- tabml/model_wrappers.py
+++ tabml/model_wrappers.py
@@ -1,7 +1,8 @@
 from abc import ABC, abstractmethod
-from typing import Iterable
+from typing import Any, Dict, Iterable
 
 from lightgbm import LGBMClassifier, LGBMRegressor
+from pytorch_tabnet.tab_model import TabNetClassifier, TabNetRegressor
 
 from tabml.utils import utils
 from tabml.utils.pb_helpers import pb_to_dict
@@ -92,3 +93,68 @@ class LgbmClassifierModelWrapper(BaseLgbmModelWrapper):
 class LgbmRegressorModelWrapper(BaseLgbmModelWrapper):
     def build_model(self):
         return LGBMRegressor(**self.params)
+
+
+class BaseSklearnModelWrapper(BaseModelWrapper):
+    def __init__(self, config, data_loader):
+        super(BaseSklearnModelWrapper, self).__init__(config, data_loader)
+        self.params = self.get_params()
+        self.model = self.build_model()
+
+    @abstractmethod
+    def build_model(self):
+        pass
+
+    @abstractmethod
+    def get_params(self):
+        pass
+
+    def predict(self, data):
+        return self.model.predict(data)
+
+    def predict_proba(self, data):
+        raise NotImplementedError
+
+    def load_model(self, model_path: str):
+        self.model = utils.load_pickle(model_path)
+
+    def show_feature_importance(self, importance_type: str = "gain"):
+        pass
+
+
+class BaseTabNetModelWrapper(BaseSklearnModelWrapper):
+    def __init__(self, config, data_loader):
+        super().__init__(config, data_loader)
+
+    def get_params(self):
+        return get_tabnet_params(self.config)
+
+
+class TabNetClassifierModelWrapper(BaseTabNetModelWrapper):
+    def build_model(self):
+        return TabNetClassifier(self.params)
+
+
+class TabNetRegressorModelWrapper(BaseTabNetModelWrapper):
+    def build_model(self):
+        return TabNetRegressor(self.params)
+
+
+def get_tabnet_params(config) -> Dict[str, Any]:
+    # Most of parameters are specified in ModelWrapperTabNetParams proto message,
+    # except for embedding params which requires features_to_model from data_loader.
+    tabnet_params = pb_to_dict(config.model_wrapper.tabnet_params)
+    features_to_model = config.data_loader.features_to_model
+    cat_features = tabnet_params["cat_features"]
+    cat_feature_names = [cat_feature["feature"] for cat_feature in cat_features]
+
+    cat_idxs = [
+        i for i, feature in enumerate(features_to_model) if feature in cat_feature_names
+    ]
+    cat_dims = [cat_feature["dim"] for cat_feature in cat_features]
+    cat_emb_dim = [cat_feature["emb_dim"] for cat_feature in cat_features]
+    del tabnet_params["cat_features"]
+    tabnet_params.update(
+        {"cat_idxs": cat_idxs, "cat_dims": cat_dims, "cat_emb_dim": cat_emb_dim}
+    )
+    return tabnet_params
