11ce58e415bed2008a4c30aefdd849b3bf4934ac
Tiep Vu <vuhuutiep@gmail.com>
2021-05-15 18:52:06 -0700

    factorization machine for ml-1m

--- tabml/movielen/datasets.py
+++ tabml/movielen/datasets.py
@@ -1,57 +1,42 @@
+from typing import List
+
 import pandas as pd
+import pandas as pd
+import torch
 from torch.utils.data import DataLoader, Dataset, random_split
+
+NUM_MOVIES = 3883
+NUM_USERS = 6040
 
 
 class FactorizationMachineDataset(Dataset):
-    def __init__(self, user_df, item_df, rating_df):
+    def __init__(self, user_df, movie_df, rating_df):
         self.user_df = user_df
-        self.item_df = item_df
-        self.rating_df = self.rating_df
-        self.age_mapping = {1: 0, 18: 1, 25: 2, 35: 3, 45: 4, 50: 5, 56: 6}
-        self.gender_mapping = {"M": 0, "F": 1}
-        self.occupation_mapping = {
-            "other": 0,
-            "academic/educator": 1,
-            "artist": 2,
-            "clerical/admin": 3,
-            "college/grad student": 4,
-            "customer service": 5,
-            "doctor/health care": 6,
-            "executive/managerial": 7,
-            "farmer": 8,
-            "homemaker": 9,
-            "K-12 student": 10,
-            "lawyer": 11,
-            "programmer": 12,
-            "retired": 13,
-            "sales/marketing": 14,
-            "scientist": 15,
-            "self-employed": 16,
-            "technician/engineer": 17,
-            "tradesman/craftsman": 18,
-            "unemployed": 19,
-            "writer": 20,
+        self.movie_df = movie_df
+        self.rating_df = rating_df
+        self.age_vocab = Vocab("data/ml-1m/ages.txt")
+        self.gender_vocab = Vocab("data/ml-1m/genders.txt")
+        self.occupation_vocab = Vocab("data/ml-1m/occupations.txt")
+        self.genre_vocab = Vocab("data/ml-1m/genres.txt")
+        self.dims = [
+            NUM_MOVIES,
+            NUM_USERS,
+            self.gender_vocab.len,
+            self.age_vocab.len,
+            self.occupation_vocab.len,
+            self.genre_vocab.len,
+        ]
+        self.input_dim = sum(self.dims)
+        self.rating_col_inds = {"user_ind": 0, "movie_ind": 1, "rating": 2}
+        self.user_col_inds = {
+            "user_ind": 0,
+            "gender": 1,
+            "age": 2,
+            "occupation": 3,
+            "zip-code": 4,
         }
-        self.genre_mapping = {
-            "Action": 0,
-            "Adventure": 1,
-            "Animation": 3,
-            "Children's": 4,
-            "Comedy": 5,
-            "Crime": 6,
-            "Documentary": 7,
-            "Drama": 8,
-            "Fantasy": 9,
-            "Film-Noir": 10,
-            "Horror": 11,
-            "Musical": 12,
-            "Mystery": 13,
-            "Romance": 14,
-            "Sci-Fi": 15,
-            "Thriller": 16,
-            "War": 17,
-            "Western": 18,
-        }
+        self.movie_col_inds = {"movie_ind": 0, "title": 1, "genres": 2}
+        # TODO (add zipcode and timestamp)
 
     def __len__(self):
         return len(self.rating_df)
@@ -60,4 +45,92 @@ class FactorizationMachineDataset(Dataset):
         """
         movie_id, movie_genre, user_id, user_gender, user_age, user_occupation
         """
-        pass
+        user_ind = self.rating_df[self.rating_col_inds["user_ind"]][ind] - 1
+        assert user_ind < NUM_USERS
+        movie_id = self.rating_df[self.rating_col_inds["movie_ind"]][ind]
+        movie_true_ind = self.find_movie_index(movie_id)
+        assert movie_true_ind < NUM_MOVIES
+        rating = self.rating_df[self.rating_col_inds["rating"]][ind]
+        gender_ind = self.gender_vocab.find_index(
+            self.user_df[self.user_col_inds["gender"]][user_ind]
+        )
+        assert gender_ind < 2
+        age_ind = self.age_vocab.find_index(
+            str(self.user_df[self.user_col_inds["age"]][user_ind])
+        )
+        assert age_ind < self.age_vocab.len
+        # occupation is given as index already
+        occupation_ind = self.user_df[self.user_col_inds["occupation"]][user_ind]
+        assert occupation_ind < self.occupation_vocab.len
+        genre_inds = []
+        genres = self.movie_df[self.movie_col_inds["genres"]][movie_true_ind].split("|")
+        if genres:
+            genre_inds = [self.genre_vocab.find_index(genre) for genre in genres]
+        inputs = self.gen_multihot_input_tensor(
+            [
+                [movie_true_ind],
+                [user_ind],
+                [gender_ind],
+                [age_ind],
+                [occupation_ind],
+                genre_inds,
+            ]
+        )
+        return inputs, rating
+
+    def find_movie_index(self, movie_id):
+        movie_inds = self.movie_df[0].tolist()
+        return movie_inds.index(movie_id)
+
+    def gen_multihot_input_tensor(self, list_inds: List[List[int]]):
+        """Generates a 1-d tensor as input of factorization machine model.
+
+        Args:
+            list_inds: a list of list of indices for each field defined in self.dims
+
+        Example:
+            If self.dims = [3, 5] and list_inds = [[2], [1, 3]] then return a tensor
+            with values: [0, 0, 1, 0, 1, 0, 1, 0].
+        """
+        assert len(list_inds) == len(
+            self.dims
+        ), f"len(list_inds) ({len(list_inds)})  != len(self.dims) ({len(self.dims)})."
+
+        offset = 0
+        sparse = torch.zeros((self.input_dim), dtype=torch.float)
+        one_inds = []
+        for i, inds in enumerate(list_inds):
+            assert not inds or all([ind < self.dims[i] for ind in inds])
+            one_inds.extend([offset + ind for ind in inds])
+            offset += self.dims[i]
+        indices = torch.LongTensor([one_inds])
+        values = torch.ones_like(torch.tensor((len(one_inds),)), dtype=torch.float)
+        sparse[indices] = values
+        return sparse
+
+
+class Vocab:
+    def __init__(self, vocab_file: str):
+        """
+        Args:
+            vocab_file: path to file containing dictionary, each line is a word.
+        """
+        self.vocab_file = vocab_file
+        self.vocab_list = self.read_vocab_file()
+        self.len = len(self.vocab_list)
+
+    def read_vocab_file(self):
+        """Returns a list of words."""
+        with open(self.vocab_file) as f:
+            words = f.read().splitlines()  # avoid newline at the end
+        return words
+
+    def find_index(self, word):
+        return self.vocab_list.index(word)
+
+
+def get_ml_1m_dataset():
+    rating_df = pd.read_csv("data/ml-1m/ratings.dat", header=None, delimiter="::")
+    user_df = pd.read_csv("data/ml-1m/users.dat", header=None, delimiter="::")
+    movie_df = pd.read_csv("data/ml-1m/movies.dat", header=None, delimiter="::")
+    return FactorizationMachineDataset(user_df, movie_df, rating_df)
--- tabml/movielen/fm.py
+++ tabml/movielen/fm.py
@@ -6,9 +6,12 @@ import torch
 import torch.multiprocessing
 import torch.nn.functional as F
 from torch import nn
+from torch.utils import data
 from torch.utils.data import DataLoader, Dataset, random_split
 from torchvision import transforms
 from torchvision.datasets import MNIST
+
+from tabml.movielen import datasets
 
 torch.multiprocessing.set_sharing_strategy("file_system")
 
@@ -18,13 +21,8 @@ NUM_GENRES = 19
 
 
 class FactorizationMachine(pl.LightningModule):
-    def __init__(self, field_dims, num_factors):
+    def __init__(self, num_inputs, num_factors):
         super(FactorizationMachine, self).__init__()
-        num_inputs = int(sum(field_dims))
-        # self.embedding = nn.Embedding(num_inputs, num_factors)
-        # self.fc = nn.Embedding(num_inputs, 1)
-        # self.linear_layer = nn.Embedding(num_inputs, 1)
-        # self.embedding = nn.Linear(num_inputs, num_factors)
         self.embedding = nn.Parameter(
             torch.randn(num_inputs, num_factors), requires_grad=True
         )
@@ -32,20 +30,11 @@ class FactorizationMachine(pl.LightningModule):
 
     def forward(self, x):
         out_1 = torch.matmul(x, self.embedding).pow(2).sum(1, keepdim=True)  # S_1^2
-        out_2 = torch.matmul(x.pow(2), self.embedding.pow(2)).sum(
-            1, keepdim=True
-        )  # S_2
+        out_2 = torch.matmul(x.pow(2), self.embedding.pow(2)).sum(1, keepdim=True)
 
         out_inter = 0.5 * (out_1 - out_2)
         out_lin = self.linear_layer(x)
         out = out_inter + out_lin
-        # square_of_sum = torch.sum(self.embedding(x), axis=1) ** 2
-        # sum_of_square = torch.sum(self.embedding(x) ** 2, axis=1)
-        # # breakpoint()
-        # tmp = torch.matmul(
-        #     self.fc(x).reshape((1, -1)), self.linear_layer(x).reshape((-1, 1))
-        # )
-        # x = tmp + 0.5 * (square_of_sum - sum_of_square).sum(1, keepdims=True)
 
         return torch.clip(out.squeeze(), min=1, max=5)
 
@@ -64,39 +53,6 @@ class FactorizationMachine(pl.LightningModule):
         return optimizer
 
 
-class MlDataset(Dataset):
-    def __init__(self, file_path: str, field_dims=[943, 1682, 19]):
-        self.df = pd.read_csv(file_path, delimiter="\t", header=None)
-        self.genre_df = pd.read_csv(
-            "data/ml-100k/u.item", delimiter="|", header=None, encoding="latin-1"
-        )[range(5, 24)]
-        self.field_dims = field_dims
-
-    def __len__(self):
-        return len(self.df)
-
-    def __getitem__(self, index):
-        # user_onehot, movie_onehot, genre_onehot
-        # return self.df[0][index] - 1, self.df[1][index] - 1, self.df[2][index]
-        user_index = self.df[0][index] - 1
-        item_index = self.df[1][index] - 1
-        item_index_offset = NUM_USERS + item_index
-        genre_osset = NUM_USERS + NUM_MOVIES
-        genre_indices = [
-            genre_osset + i
-            for i, val in enumerate(self.genre_df.iloc[item_index].tolist())
-            if val == 1
-        ]
-        inputs = [user_index, item_index_offset] + genre_indices
-
-        sparse = torch.zeros([NUM_USERS + NUM_MOVIES + NUM_GENRES], dtype=torch.float)
-        indices = torch.LongTensor([inputs])
-        values = torch.ones_like(torch.tensor((len(inputs),)), dtype=torch.float)
-        sparse[indices] = values
-
-        return sparse, self.df[2][index]
-
-
 def eval_model(model, train_dataloader):
     loss = 0
     for inputs, rating in train_dataloader:
@@ -107,19 +63,19 @@ def eval_model(model, train_dataloader):
 
 
 def run_pipeline():
-    training_data = MlDataset("data/ml-100k/u1.base")
-    validation_data = MlDataset("data/ml-100k/u1.test")
-    batch_size = 256
+    training_data = datasets.get_ml_1m_dataset()
+    # TODO: split training dataset
+    validation_data = training_data  # for now
+    batch_size = 1024
+    n_factors = 30
     train_dataloader = DataLoader(
-        training_data, batch_size=batch_size, shuffle=True, num_workers=10
+        training_data, batch_size=batch_size, shuffle=True, num_workers=12
     )
     validation_dataloader = DataLoader(
-        validation_data, batch_size=batch_size, shuffle=False, num_workers=10
+        validation_data, batch_size=batch_size, shuffle=False, num_workers=12
     )
-    # https://files.grouplens.org/datasets/movielens/ml-100k-README.txt
-    n_factors = 30
     model = FactorizationMachine(
-        field_dims=[NUM_USERS, NUM_MOVIES, NUM_GENRES], num_factors=n_factors
+        num_inputs=training_data.input_dim, num_factors=n_factors
     )
     trainer = pl.Trainer(gpus=1, max_epochs=10)
     trainer.fit(model, train_dataloader, validation_dataloader)
@@ -131,4 +87,3 @@ def run_pipeline():
 
 if __name__ == "__main__":
     run_pipeline()
-    # run_pipeline2()
