51433b74db8165d41e4bb98fdce8a005d9c56b4d
tiepvupsu <vuhuutiep@gmail.com>
2021-07-05 16:39:45 -0700

    Pypi (#62)
    * add setup.cfg
    * update setup

--- setup.py
+++ setup.py
@@ -1,21 +1,16 @@
-from setuptools import find_packages, setup
-
-with open("README.md") as f:
-    readme = f.read()
-
-with open("LICENSE") as f:
-    license = f.read()
+from distutils.core import setup
 
 setup(
-    name="tabml",
-    version="0.1",
-    description="Machine Learning with Tabular data",
-    long_description=readme,
-    author="Tiep Vu",
-    author_email="vuhuutiep@gmail.com",
-    url="https://github.com/tiepvupsu/tabml",
-    license=license,
-    packages=find_packages(exclude=("tests", "docs")),
+    name="tabml",  # How you named your package folder (MyLib)
+    packages=["tabml"],  # Chose the same as "name"
+    version="0.1",  # Start with a small number and increase it with every change you make
+    license="apache-2.0",  # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+    description="A package for machine learning with tabular data",  # Give a short description about your library
+    author="Tiep Vu",  # Type in your name
+    author_email="vuhuutiep@gmail.com",  # Type in your E-Mail
+    url="https://github.com/tiepvupsu/tabml",  # Provide either the link to your github or to your website
+    download_url="https://github.com/tiepvupsu/tabml/archive/refs/tags/v_01.tar.gz",  # I explain this later on
+    keywords=["Machine Learning", "Tabular"],  # Keywords that define your package best
     install_requires=[
         "GitPython>=3.1.13",
         "GPUtil>=1.4.0",
@@ -28,5 +23,12 @@ setup(
         "protobuf>=3.13.0",
         "scikit-learn>=0.24.2",
         "scipy>=1.6.2",
+    ],  # I get to this in a second
+    classifiers=[
+        "Development Status :: 3 - Alpha",  # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+        "Intended Audience :: Developers",  # Define that your audience are developers
+        "Topic :: Software Development :: Build Tools",
+        "License :: OSI Approved :: Apache Software License",  # Again, pick a license
+        "Programming Language :: Python :: 3.8",
     ],
 )
