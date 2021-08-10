e3ad090a638974a1ee701f1f6599dad7d6c92f7b
Tiep Vu <vuhuutiep@gmail.com>
2021-08-10 12:47:41 -0700

    change path dir in titanic test

--- examples/titanic/test_pipelines.py
+++ examples/titanic/test_pipelines.py
@@ -1,4 +1,11 @@
+import os
+import sys
+
 from . import pipelines
+
+TEST_DIR = os.path.dirname(os.path.abspath(__file__))
+PROJECT_DIR = os.path.abspath(os.path.join(TEST_DIR, os.pardir))
+sys.path.insert(0, PROJECT_DIR)
 
 
 def test_full_pipeline():
