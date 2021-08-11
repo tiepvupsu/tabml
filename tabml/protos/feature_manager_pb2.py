81db8c681b42aa6137faeecf6316ca84bc749bac
Tiep Vu <vuhuutiep@gmail.com>
2021-08-10 17:29:12 -0700

    remove path.proto, use string instead

--- tabml/protos/feature_manager_pb2.py
+++ tabml/protos/feature_manager_pb2.py
@@ -12,7 +12,6 @@ from google.protobuf import symbol_database as _symbol_database
 _sym_db = _symbol_database.Default()
 
 
-from tabml.protos import path_pb2 as tabml_dot_protos_dot_path__pb2
 
 
 DESCRIPTOR = _descriptor.FileDescriptor(
@@ -21,9 +20,8 @@ DESCRIPTOR = _descriptor.FileDescriptor(
   syntax='proto2',
   serialized_options=None,
   create_key=_descriptor._internal_create_key,
-  serialized_pb=b'\n\"tabml/protos/feature_manager.proto\x12\x0ctabml.protos\x1a\x17tabml/protos/path.proto\"?\n\x0b\x42\x61seFeature\x12\x0c\n\x04name\x18\x01 \x02(\t\x12\"\n\x05\x64type\x18\x02 \x02(\x0e\x32\x13.tabml.protos.DType\"t\n\x13TransformingFeature\x12\r\n\x05index\x18\x01 \x02(\x03\x12\x0c\n\x04name\x18\x02 \x02(\t\x12\x14\n\x0c\x64\x65pendencies\x18\x03 \x03(\t\x12*\n\x05\x64type\x18\x04 \x01(\x0e\x32\x13.tabml.protos.DType:\x06STRING\"\xc3\x01\n\rFeatureConfig\x12(\n\x0craw_data_dir\x18\x01 \x01(\x0b\x32\x12.tabml.protos.Path\x12\x14\n\x0c\x64\x61taset_name\x18\x02 \x01(\t\x12\x30\n\rbase_features\x18\x03 \x03(\x0b\x32\x19.tabml.protos.BaseFeature\x12@\n\x15transforming_features\x18\x04 \x03(\x0b\x32!.tabml.protos.TransformingFeature*l\n\x05\x44Type\x12\x08\n\x04\x42OOL\x10\x01\x12\t\n\x05INT32\x10\x02\x12\t\n\x05INT64\x10\x03\x12\n\n\x06STRING\x10\x04\x12\t\n\x05\x46LOAT\x10\x05\x12\n\n\x06\x44OUBLE\x10\x06\x12\x08\n\x04\x44\x41TE\x10\x07\x12\x08\n\x04TIME\x10\x08\x12\x0c\n\x08\x44\x41TETIME\x10\t'
-  ,
-  dependencies=[tabml_dot_protos_dot_path__pb2.DESCRIPTOR,])
+  serialized_pb=b'\n\"tabml/protos/feature_manager.proto\x12\x0ctabml.protos\"?\n\x0b\x42\x61seFeature\x12\x0c\n\x04name\x18\x01 \x02(\t\x12\"\n\x05\x64type\x18\x02 \x02(\x0e\x32\x13.tabml.protos.DType\"t\n\x13TransformingFeature\x12\r\n\x05index\x18\x01 \x02(\x03\x12\x0c\n\x04name\x18\x02 \x02(\t\x12\x14\n\x0c\x64\x65pendencies\x18\x03 \x03(\t\x12*\n\x05\x64type\x18\x04 \x01(\x0e\x32\x13.tabml.protos.DType:\x06STRING\"\xaf\x01\n\rFeatureConfig\x12\x14\n\x0craw_data_dir\x18\x01 \x01(\t\x12\x14\n\x0c\x64\x61taset_name\x18\x02 \x01(\t\x12\x30\n\rbase_features\x18\x03 \x03(\x0b\x32\x19.tabml.protos.BaseFeature\x12@\n\x15transforming_features\x18\x04 \x03(\x0b\x32!.tabml.protos.TransformingFeature*l\n\x05\x44Type\x12\x08\n\x04\x42OOL\x10\x01\x12\t\n\x05INT32\x10\x02\x12\t\n\x05INT64\x10\x03\x12\n\n\x06STRING\x10\x04\x12\t\n\x05\x46LOAT\x10\x05\x12\n\n\x06\x44OUBLE\x10\x06\x12\x08\n\x04\x44\x41TE\x10\x07\x12\x08\n\x04TIME\x10\x08\x12\x0c\n\x08\x44\x41TETIME\x10\t'
+)
 
 _DTYPE = _descriptor.EnumDescriptor(
   name='DType',
@@ -80,8 +78,8 @@ _DTYPE = _descriptor.EnumDescriptor(
   ],
   containing_type=None,
   serialized_options=None,
-  serialized_start=458,
-  serialized_end=566,
+  serialized_start=413,
+  serialized_end=521,
 )
 _sym_db.RegisterEnumDescriptor(_DTYPE)
 
@@ -132,8 +130,8 @@ _BASEFEATURE = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=77,
-  serialized_end=140,
+  serialized_start=52,
+  serialized_end=115,
 )
 
 
@@ -185,8 +183,8 @@ _TRANSFORMINGFEATURE = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=142,
-  serialized_end=258,
+  serialized_start=117,
+  serialized_end=233,
 )
 
 
@@ -200,8 +198,8 @@ _FEATURECONFIG = _descriptor.Descriptor(
   fields=[
     _descriptor.FieldDescriptor(
       name='raw_data_dir', full_name='tabml.protos.FeatureConfig.raw_data_dir', index=0,
-      number=1, type=11, cpp_type=10, label=1,
-      has_default_value=False, default_value=None,
+      number=1, type=9, cpp_type=9, label=1,
+      has_default_value=False, default_value=b"".decode('utf-8'),
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
@@ -238,13 +236,12 @@ _FEATURECONFIG = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=261,
-  serialized_end=456,
+  serialized_start=236,
+  serialized_end=411,
 )
 
 _BASEFEATURE.fields_by_name['dtype'].enum_type = _DTYPE
 _TRANSFORMINGFEATURE.fields_by_name['dtype'].enum_type = _DTYPE
-_FEATURECONFIG.fields_by_name['raw_data_dir'].message_type = tabml_dot_protos_dot_path__pb2._PATH
 _FEATURECONFIG.fields_by_name['base_features'].message_type = _BASEFEATURE
 _FEATURECONFIG.fields_by_name['transforming_features'].message_type = _TRANSFORMINGFEATURE
 DESCRIPTOR.message_types_by_name['BaseFeature'] = _BASEFEATURE
