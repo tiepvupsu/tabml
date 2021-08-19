34f16a9d156c61c4b3b94ac5e94afeb137b64cb8
Tiep Vu <vuhuutiep@gmail.com>
2021-08-18 23:44:56 -0700

    add CategoryEncoder and tests

--- tests/test_data_processing.py
+++ tests/test_data_processing.py
@@ -1,7 +1,9 @@
 import pandas as pd
+import pytest
 from qcore.asserts import assert_eq
 
 from tabml import data_processing
+from tabml.utils.utils import write_str_to_file
 
 
 class TestQuantileClipper:
@@ -47,3 +49,56 @@ class TestCrossColumns:
         expected = pd.Series(data=["x_X_1", "y_X_2", "z_X_3"])
         got = data_processing.cross_columns(df, cols=["a", "b"])
         pd.testing.assert_series_equal(expected, got, check_names=False)
+
+
+class TestCategoryEncoder:
+    @pytest.fixture(autouse=True)
+    def setup_class(self, tmp_path):
+        # self.pb_config_path = tmp_path / "pipeline_config.pb"
+        # write_str_to_file(pb_str, self.pb_config_path)
+        self.series = pd.Series(["a", "b", "c", "d"])
+
+    def test_1(self):
+        vocab_map = {"a": 0, "b": 1, "c": 2, "d": 3}
+        got = data_processing.CategoryEncoder(vocab_map).get_encoded(self.series)
+        expected = pd.Series([0, 1, 2, 3])
+        pd.testing.assert_series_equal(expected, got)
+
+    def test_unknown(self):
+        vocab_map = {"a": 0, "b": 1, "c": 5}
+        got = data_processing.CategoryEncoder(vocab_map).get_encoded(self.series)
+        expected = pd.Series([0, 1, 5, 6])
+        pd.testing.assert_series_equal(expected, got)
+
+    def test_from_list(self):
+        vocab = ["a", "c", "b"]
+        got = data_processing.CategoryEncoder.from_list(vocab).get_encoded(self.series)
+        expected = pd.Series([0, 2, 1, 3])
+        pd.testing.assert_series_equal(expected, got)
+
+    def test_from_lines_in_txt(self, tmp_path):
+        file_content = """a
+            d
+            c
+        """
+        txt_path = tmp_path / "foo.txt"
+        write_str_to_file(file_content, txt_path)
+        got = data_processing.CategoryEncoder.from_lines_in_txt(txt_path).get_encoded(
+            self.series
+        )
+        expected = pd.Series([0, 3, 2, 1])
+        pd.testing.assert_series_equal(expected, got)
+
+    def test_from_mapping_in_csv(self, tmp_path):
+        file_content = """
+            a,0
+            d, 2
+            c,5
+        """
+        txt_path = tmp_path / "bar.txt"
+        write_str_to_file(file_content, txt_path)
+        got = data_processing.CategoryEncoder.from_mapping_in_csv(txt_path).get_encoded(
+            self.series
+        )
+        expected = pd.Series([0, 6, 5, 2])
+        pd.testing.assert_series_equal(expected, got)
