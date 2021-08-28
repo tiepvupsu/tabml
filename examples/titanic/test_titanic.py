fd443e447ce6ece9ba99980db55a2fe516d12a0d
Tiep Vu <vuhuutiep@gmail.com>
2021-08-28 16:42:59 -0700

    titanic: add placeholder for testing model inference

--- examples/titanic/test_titanic.py
+++ examples/titanic/test_titanic.py
@@ -1,3 +1,7 @@
+from pathlib import Path
+
+from tabml.experiment_manager import ExperimentManger
+from tabml.inference import ModelInference
 from tabml.utils.utils import change_working_dir_pytest
 
 from . import feature_manager, pipelines
@@ -14,46 +18,29 @@ def test_full_pipeline():
 
 
 @change_working_dir_pytest
-def test_fit_example():
-    pb_config_path = "configs/feature_config.pb"
-    fm = feature_manager.FeatureManager(pb_config_path)
-    raw_data_samples = [
-        {
-            "PassengerId": 1,
-            "Pclass": 1,
-            "Name": "First, Mr. Last",
-            "Sex": "female",
-            "SibSp": 3,
-            "Parch": 0,
-            "Ticket": 12345,
-            "Fare": 10.0,
-            "Cabin": None,
-            "Embarked": "C",
-        },
-        {
-            "PassengerId": 2,
-            "Pclass": 1,
-            "Name": "First, Mrs. Last",
-            "Sex": "male",
-            "Age": 60,
-            "SibSp": 0,
-            "Parch": 2,
-            "Ticket": 12345,
-            "Fare": 100.0,
-            "Cabin": None,
-            "Embarked": "Q",
-        },
-    ]
-    transforming_features = [
-        "imputed_age",
-        "bucketized_age",
-        "min_max_scaled_age",
-        "title",
-        "coded_title",
-        "coded_sex",
-    ]
-    print(
-        fm.transform_new_samples(
-            raw_data_samples, transforming_features=transforming_features
-        )
+def test_inference():
+    feature_config_path = "./configs/feature_config.pb"
+    lgbm_config_path = "./configs/lgbm_config.pb"
+    em = ExperimentManger(lgbm_config_path)
+    last_model_run_dir = em.get_most_recent_run_dir()
+    model_path = Path(last_model_run_dir) / "model_0"
+    model_inference = ModelInference(
+        feature_config_path=feature_config_path,
+        pipeline_config_path=lgbm_config_path,
+        model_path=model_path,
     )
+    raw_data = {
+        "PassengerId": 1,
+        "Pclass": 1,
+        "Name": "First, Dr. Last",
+        "Sex": "female",
+        "Age": 30,
+        "SibSp": 3,
+        "Parch": 0,
+        "Ticket": 12345,
+        "Fare": 10.0,
+        "Cabin": None,
+        "Embarked": "C",
+    }
+    model_inference.predict_one_sample(raw_data)
+    model_inference.predict_one_batch([raw_data, raw_data])
