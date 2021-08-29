0cad8c904f55ce9ff2a240e804c8f823127090e0
Tiep Vu <vuhuutiep@gmail.com>
2021-08-29 11:35:23 -0700

    clean inference.py

--- tabml/inference.py
+++ tabml/inference.py
@@ -2,6 +2,7 @@
 
 from typing import Any, Dict, List
 
+from tabml.feature_manager import BaseFeatureManager
 from tabml.pipelines import BasePipeline
 
 
@@ -9,34 +10,31 @@ class ModelInference:
     def __init__(
         self,
         feature_config_path: str,
-        feature_manager_cls,
+        feature_manager_cls: BaseFeatureManager,
         pipeline_config_path: str,
         model_path: str = None,
     ):
         self.fm = feature_manager_cls(feature_config_path)
         self.pipeline = BasePipeline(pipeline_config_path)
         self.pipeline.model_wrapper.load_model(model_path)
-        self.features_to_model = list(
-            self.pipeline.config.data_loader.features_to_model
-        )
 
     @classmethod
     def init_from_model_path(cls, model_path):
         """
-        pipeline_config_path could be inferred from model_path
+        feature_config_path and pipelien_config_path could be inferred from model_path
         """
         # TODO: implement me
         NotImplementedError
 
     def predict(self, raw_data: List[Dict[str, Any]]) -> List[Any]:
-        """Makes prediction for new samples.
+        """
+        Makes prediction for new samples.
 
         The sample should be given in a form of a list of dictionaries whose keys are
-        the raw features.
-
-        The feature manager transform the raw_data into a processed dataframe that is
-        fed into the model to make predictions.
+        the raw features. The feature manager transform the raw_data into a processed dataframe that
+        is fed into the model to make predictions.
         """
-
-        features = self.fm.transform_new_samples(raw_data, self.features_to_model)
-        return self.pipeline.model_wrapper.predict(features[self.features_to_model])
+        features_to_model = list(self.pipeline.config.data_loader.features_to_model)
+        self.fm.get_raw_data_one_sample(raw_data)
+        features = self.fm.transform_new_samples(raw_data, features_to_model)
+        return self.pipeline.model_wrapper.predict(features[features_to_model])
