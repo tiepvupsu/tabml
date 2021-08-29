1c2a4b8ae8f02347141b5f8e688fc63c7b841954
Tiep Vu <vuhuutiep@gmail.com>
2021-08-29 11:31:58 -0700

    small fixes in inference

--- tabml/inference.py
+++ tabml/inference.py
@@ -16,6 +16,9 @@ class ModelInference:
         self.fm = feature_manager_cls(feature_config_path)
         self.pipeline = BasePipeline(pipeline_config_path)
         self.pipeline.model_wrapper.load_model(model_path)
+        self.features_to_model = list(
+            self.pipeline.config.data_loader.features_to_model
+        )
 
     @classmethod
     def init_from_model_path(cls, model_path):
@@ -26,8 +29,7 @@ class ModelInference:
         NotImplementedError
 
     def predict(self, raw_data: List[Dict[str, Any]]) -> List[Any]:
-        """
-        Makes prediction for new samples.
+        """Makes prediction for new samples.
 
         The sample should be given in a form of a list of dictionaries whose keys are
         the raw features.
@@ -36,6 +38,5 @@ class ModelInference:
         fed into the model to make predictions.
         """
 
-        features_to_model = list(self.pipeline.config.data_loader.features_to_model)
-        features = self.fm.transform_new_samples(raw_data, features_to_model)
-        return self.pipeline.model_wrapper.predict(features[features_to_model])
+        features = self.fm.transform_new_samples(raw_data, self.features_to_model)
+        return self.pipeline.model_wrapper.predict(features[self.features_to_model])
