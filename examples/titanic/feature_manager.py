b3a05d825accce8f3346aead5d854c3b89ece37b
Tiep Vu <vuhuutiep@gmail.com>
2021-08-29 10:43:51 -0700

    cleanup titanic/feature_manager

--- examples/titanic/feature_manager.py
+++ examples/titanic/feature_manager.py
@@ -177,52 +177,5 @@ def run():
     fm.run_all()
 
 
-def fit_example():
-    pb_config_path = "configs/feature_config.pb"
-    fm = FeatureManager(pb_config_path)
-    raw_data_samples = [
-        {
-            "PassengerId": 1,
-            "Pclass": 1,
-            "Name": "First, Mr. Last",
-            "Sex": "female",
-            # "Age": 30,
-            "SibSp": 3,
-            "Parch": 0,
-            "Ticket": 12345,
-            "Fare": 10.0,
-            "Cabin": None,
-            "Embarked": "C",
-        },
-        {
-            "PassengerId": 2,
-            "Pclass": 1,
-            "Name": "First, Mrs. Last",
-            "Sex": "male",
-            "Age": 60,
-            "SibSp": 0,
-            "Parch": 2,
-            "Ticket": 12345,
-            "Fare": 100.0,
-            "Cabin": None,
-            "Embarked": "Q",
-        },
-    ]
-    transforming_features = [
-        "imputed_age",
-        "bucketized_age",
-        "min_max_scaled_age",
-        "title",
-        "coded_title",
-        "coded_sex",
-    ]
-    print(
-        fm.transform_new_samples(
-            raw_data_samples, transforming_features=transforming_features
-        )
-    )
-
-
 if __name__ == "__main__":
-    # run()
-    fit_example()
+    run()
