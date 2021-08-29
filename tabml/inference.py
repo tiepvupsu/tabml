684ea7bc717d323d53350e398b3ce86ac2f95083
Tiep Vu <vuhuutiep@gmail.com>
2021-08-29 11:44:41 -0700

    update inferece

--- tabml/inference.py
+++ tabml/inference.py
@@ -2,32 +2,21 @@
 
 from typing import Any, Dict, List
 
-from tabml.feature_manager import BaseFeatureManager
+from tabml.experiment_manager import ExperimentManger
 from tabml.pipelines import BasePipeline
 
 
 class ModelInference:
-    def __init__(
-        self,
-        feature_config_path: str,
-        feature_manager_cls: BaseFeatureManager,
-        pipeline_config_path: str,
-        model_path: str = None,
-    ):
+    def __init__(self, feature_manager_cls, feature_config_path: str, model_path: str):
         self.fm = feature_manager_cls(feature_config_path)
+        pipeline_config_path = ExperimentManger.get_config_path_from_model_path(
+            model_path
+        )
         self.pipeline = BasePipeline(pipeline_config_path)
         self.pipeline.model_wrapper.load_model(model_path)
         self.features_to_model = list(
             self.pipeline.config.data_loader.features_to_model
         )
-
-    @classmethod
-    def init_from_model_path(cls, model_path):
-        """
-        feature_config_path and pipelien_config_path could be inferred from model_path
-        """
-        # TODO: implement me
-        NotImplementedError
 
     def predict(self, raw_data: List[Dict[str, Any]]) -> List[Any]:
         """
