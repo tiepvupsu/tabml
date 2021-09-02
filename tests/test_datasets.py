a083a703505e13f7ce7a0c85b499ad3f0aa617cc
Tiep Vu <vuhuutiep@gmail.com>
2021-09-01 21:46:02 -0700

    Version 0.1.9

--- tests/test_datasets.py
+++ tests/test_datasets.py
@@ -1,13 +1,5 @@
 from tabml import datasets
 
 
-def test_download_titanic():
-    datasets.download_as_dataframes("titanic")
-
-
-def test_download_california_housing():
-    datasets.download_as_dataframes("california_housing")
-
-
 def test_download_movielen_1m():
     datasets.download_as_dataframes("movielen-1m")
