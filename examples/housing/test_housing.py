369cdd67932ffca72eef3fee48f1c15ccf942f85
Tiep Vu <vuhuutiep@gmail.com>
2021-09-07 19:54:45 -0700

    add housing xgboost config

--- examples/housing/test_housing.py
+++ examples/housing/test_housing.py
@@ -4,6 +4,15 @@ from . import feature_manager, pipelines
 
 
 @change_working_dir_pytest
-def test_full_pipeline():
+def test_feature_manager():
     feature_manager.run()
+
+
+@change_working_dir_pytest
+def test_full_pipeline_lgbm():
     pipelines.train_lgbm()
+
+
+@change_working_dir_pytest
+def test_full_pipeline_xgboost():
+    pipelines.train_xgboost()
