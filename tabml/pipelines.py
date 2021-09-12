9a6ae886e72f241a7faa625d8138209336a51e0c
Tiep Vu <vuhuutiep@gmail.com>
2021-09-12 16:03:03 -0700

    clean up pipelines

--- tabml/pipelines.py
+++ tabml/pipelines.py
@@ -19,8 +19,6 @@ class BasePipeline(ABC):
             (passed via path_to_config when initialized)
         data_loader:
             a data loader object, a subclass of base.data_loaders.BaseDataLoader.
-        trainer:
-            a trainer object, a subclass of base.trainers.BaseTrainer.
         exp_manager:
             a base.experiment_manager.ExperimentManager object.
         custom_model_wrapper:
@@ -40,7 +38,6 @@ class BasePipeline(ABC):
         self.data_loader = self._get_data_loader()
         assert self.data_loader.label_col is not None, "label_col must be specified"
         self._get_model_wrapper(custom_model_wrapper)
-        # self.trainer = self._get_trainer()
 
         logger.add(self.exp_manager.get_log_path())
 
@@ -70,11 +67,6 @@ class BasePipeline(ABC):
             self.model_wrapper = factory.create(self.config.model_wrapper.cls_name)(
                 self.config
             )
-
-    # def _get_trainer(self) -> BaseTrainer:
-    #     return factory.create(self.config.trainer.cls_name)(
-    #         self.model_wrapper, self.data_loader, self.config
-    #     )
 
     def analyze_model(self) -> None:
         """Analyze the model on the validation dataset.
