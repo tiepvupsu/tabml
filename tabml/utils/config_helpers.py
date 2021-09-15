a5d14c7ee12e1d47768cd8be76502747822b6d42
Tiep Vu <vuhuutiep@gmail.com>
2021-09-14 17:05:09 -0700

    update config_helpers.py to work with yaml feature config

--- tabml/utils/config_helpers.py
+++ tabml/utils/config_helpers.py
@@ -21,10 +21,25 @@ def pb_to_dict(config):
     return json_format.MessageToDict(config, preserving_proto_field_name=True)
 
 
-def parse_pipeline_config_yaml(pipeline_yaml_path: str):
-    with open(pipeline_yaml_path) as f:
+def parse_config_yaml(yaml_path: str):
+    with open(yaml_path) as f:
         config = yaml.load(f, Loader=yaml.BaseLoader)  # config is dict
         return AttrDict(config)
+
+
+def parse_feature_config(config_path: str):
+    ext = config_path.split(".")[-1]
+    assert ext in (
+        "pb",
+        "yaml",
+        "yml",
+    ), f"feature_path {config_path} extension not supported."
+    if ext == "pb":
+        return parse_feature_config_pb(config_path)
+    elif ext in ("yaml", "yml"):
+        return parse_config_yaml(config_path)
+    else:
+        ValueError(f"pipeline_path {config_path} extension not supported.")
 
 
 def parse_pipeline_config(pipeline_path: str):
@@ -37,7 +52,7 @@ def parse_pipeline_config(pipeline_path: str):
     if ext == "pb":
         return parse_pipeline_config_pb(pipeline_path)
     elif ext in ("yaml", "yml"):
-        return parse_pipeline_config_yaml(pipeline_path)
+        return parse_config_yaml(pipeline_path)
     else:
         ValueError(f"pipeline_path {pipeline_path} extension not supported.")
 
