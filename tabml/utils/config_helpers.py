5c3b4b7c8b9f82747455eaa1af2a14bb2d066958
Tiep Vu <vuhuutiep@gmail.com>
2021-09-15 00:22:15 -0700

    fix test_feature_manager

--- tabml/utils/config_helpers.py
+++ tabml/utils/config_helpers.py
@@ -2,13 +2,7 @@ import yaml
 from attrdict import AttrDict
 from google.protobuf import json_format, text_format
 
-from tabml.protos import feature_manager_pb2, pipeline_pb2
-
-
-def parse_feature_config_pb(feature_manager_pbtxt_path: str):
-    with open(feature_manager_pbtxt_path, "r") as file_object:
-        config = feature_manager_pb2.FeatureConfig()
-        return text_format.MergeLines(file_object, config)
+from tabml.protos import pipeline_pb2
 
 
 def parse_pipeline_config_pb(pipeline_pbtxt_path: str):
@@ -30,16 +24,10 @@ def parse_config_yaml(yaml_path: str):
 def parse_feature_config(config_path: str):
     ext = config_path.split(".")[-1]
     assert ext in (
-        "pb",
         "yaml",
         "yml",
     ), f"feature_path {config_path} extension not supported."
-    if ext == "pb":
-        return parse_feature_config_pb(config_path)
-    elif ext in ("yaml", "yml"):
-        return parse_config_yaml(config_path)
-    else:
-        ValueError(f"pipeline_path {config_path} extension not supported.")
+    return parse_config_yaml(config_path)
 
 
 def parse_pipeline_config(pipeline_path: str):
