a27b956c471f7ff5199d28932802d03990bb937c
Tiep Vu <vuhuutiep@gmail.com>
2021-09-26 15:08:08 -0700

    pipeline_config.DataLoader as input of BaseDataLoader

--- tabml/model_analysis.py
+++ tabml/model_analysis.py
@@ -107,8 +107,7 @@ class ModelAnalysis:
         )
         if dataset_name == "train":
             df = self.data_loader.feature_manager.extract_dataframe(
-                features_to_select=all_features,
-                filters=self.data_loader.config.data_loader.train_filters,
+                features_to_select=all_features, filters=self.data_loader.train_filters
             )
             if self.training_size:
                 return df.sample(n=min(int(self.training_size), len(df)))
@@ -117,7 +116,7 @@ class ModelAnalysis:
         if dataset_name == "val":
             return self.data_loader.feature_manager.extract_dataframe(
                 features_to_select=all_features,
-                filters=self.data_loader.config.data_loader.validation_filters,
+                filters=self.data_loader.validation_filters,
             )
         raise ValueError(f"dataset_name ({dataset_name}) not in ('train', 'val')")
 
