f02cee522421ab2d72b3345d80592bf0e35ac4ea
Tiep Vu <vuhuutiep@gmail.com>
2021-10-18 18:04:29 -0700

    replace yaml.load by yaml.safe_load()

--- tabml/config_helpers.py
+++ tabml/config_helpers.py
@@ -5,11 +5,11 @@ from tabml.schemas import feature_config, pipeline_config
 
 def parse_feature_config(yaml_path: str):
     with open(yaml_path) as f:
-        config = yaml.load(f)  # config is dict
+        config = yaml.safe_load(f)
         return feature_config.FeatureConfig(**config)
 
 
 def parse_pipeline_config(yaml_path: str):
     with open(yaml_path) as f:
-        config = yaml.load(f)  # config is dict
+        config = yaml.safe_load(f)
         return pipeline_config.PipelineConfig(**config)
