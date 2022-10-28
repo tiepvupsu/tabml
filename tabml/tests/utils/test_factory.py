f0199a551eb3e274e309f93bc4c9856e9ba4d362
Tiep Vu <vuhuutiep@gmail.com>
2022-10-28 12:26:12 -0700

    Fix other files

--- tabml/tests/utils/test_factory.py
+++ tabml/tests/utils/test_factory.py
@@ -1,5 +1,3 @@
-from qcore.asserts import assert_eq
-
 from tabml.feature_manager import BaseFeatureManager
 from tabml.utils import factory
 
@@ -8,4 +6,4 @@ class TestCreate:
     def test_1(self):
         cls_name = "tabml.feature_manager.BaseFeatureManager"
         got = factory.create(cls_name)
-        assert_eq(BaseFeatureManager, got)
+        assert BaseFeatureManager == got
