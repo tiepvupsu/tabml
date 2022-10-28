2f854d7bb185e4208a8866a0f3b814763d864f5b
Tiep Vu <vuhuutiep@gmail.com>
2022-10-28 14:48:25 -0700

    release 0.2.0

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ long_description = (this_directory / "README.md").read_text()
 setup(
     name="tabml",
     packages=setuptools.find_packages(),
-    version="0.1.19",
+    version="0.2.0",
     license="apache-2.0",
     description="A package for machine learning with tabular data",
     author="Tiep Vu",
