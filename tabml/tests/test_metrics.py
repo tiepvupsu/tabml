cfb17ff99e15227da0e852f3899a1730176ab444
Tiep Vu <vuhuutiep@gmail.com>
2022-10-28 12:20:37 -0700

    fix test metrics

--- tabml/tests/test_metrics.py
+++ tabml/tests/test_metrics.py
@@ -1,7 +1,7 @@
 import typing
 
 import numpy as np
-from qcore.asserts import AssertRaises, assert_eq
+import pytest
 
 from tabml import metrics
 
@@ -40,37 +40,23 @@ class DummyMetric3(metrics.BaseMetric):
 
 class TestBaseMetric:
     def test_raise_ve_if_is_higher_better_not_defind(self):
-        with AssertRaises(ValueError) as assert_raises:
+        with pytest.raises(ValueError) as excinfo:
             DummyMetric1().compute_scores([0], [0])
 
-        error_message = assert_raises.expected_exception_found
-        assert_eq(
-            True,
-            error_message.args[0].startswith(
-                "Subclasses must define is_higher_better."
-            ),
-        )
+        assert str(excinfo.value).startswith("Subclasses must define is_higher_better.")
 
     def test_raise_ve_if_labels_and_preds_have_diff_lens(self):
-        with AssertRaises(ValueError) as assert_raises:
+        with pytest.raises(ValueError) as excinfo:
             DummyMetric2().compute_scores([0, 0], [0])
 
-        error_message = assert_raises.expected_exception_found
-        assert_eq(
-            True,
-            error_message.args[0].startswith("labels (len = 2) and preds (len = 1)"),
-        )
+        assert str(excinfo.value).startswith("labels (len = 2) and preds (len = 1)")
 
     def test_score_names_and_compute_scores_have_diff_lens(self):
-        with AssertRaises(ValueError) as assert_raises:
+        with pytest.raises(ValueError) as excinfo:
             DummyMetric3().compute_scores([0], [0])
 
-        error_message = assert_raises.expected_exception_found
-        assert_eq(
-            True,
-            error_message.args[0].startswith(
-                "self.score_names (['a']) and scores ([0, 0]) must have the same length"
-            ),
+        assert str(excinfo.value).startswith(
+            "self.score_names (['a']) and scores ([0, 0]) must have the same length"
         )
 
 
@@ -78,15 +64,11 @@ class TestComputeMaxF1AndThreshold:
     def test_raise_if_labels_not_binary(self):
         labels = [0, 2]
         preds = [0, 1]
-        with AssertRaises(ValueError) as assert_raises:
+        with pytest.raises(ValueError) as excinfo:
             metrics.compute_maxf1_stats(labels, preds)
 
-        error_message = assert_raises.expected_exception_found
-        assert_eq(
-            True,
-            error_message.args[0].startswith(
-                "Input must contain only binary values, got "
-            ),
+        assert str(excinfo.value).startswith(
+            "Input must contain only binary values, got "
         )
 
     def test_1(self):
@@ -99,10 +81,10 @@ class TestComputeMaxF1AndThreshold:
         max_f1, threshold, precision, recall = metrics.compute_maxf1_stats(
             labels, preds
         )
-        assert_eq(expected_max_f1, max_f1)
-        assert_eq(expected_max_f1_threshold, threshold, tolerance=1e-6)
-        assert_eq(expected_max_f1_precision, precision)
-        assert_eq(expected_max_f1_recall, recall)
+        assert expected_max_f1 == max_f1
+        assert expected_max_f1_threshold == pytest.approx(threshold)
+        assert expected_max_f1_precision == precision
+        assert expected_max_f1_recall == recall
 
     def test_2(self):
         labels = [0, 0, 1, 1]
@@ -111,16 +93,16 @@ class TestComputeMaxF1AndThreshold:
         expected_max_f1_precision = 1 / 2
         expected_max_f1_recall = 1
         max_f1, _, precision, recall = metrics.compute_maxf1_stats(labels, preds)
-        assert_eq(expected_max_f1, max_f1)
-        assert_eq(expected_max_f1_precision, precision)
-        assert_eq(expected_max_f1_recall, recall)
+        assert expected_max_f1 == max_f1
+        assert expected_max_f1_precision == precision
+        assert expected_max_f1_recall == recall
 
 
 class TestRocAuc:
     def test_return_nan_if_uniqe_label(self):
         got = metrics.RocAreaUnderTheCurve().compute_scores([0, 0], [0.5, 0.5])
         expected = {"roc_auc": np.nan}
-        assert_eq(expected, got)
+        assert expected == got
 
 
 class TestF1Score:
@@ -129,7 +111,7 @@ class TestF1Score:
         preds = [0, 0, 1, 1, 1]
         expected = {"f1": 2 / 3, "precision": 2 / 3, "recall": 2 / 3}
         got = metrics.F1Score().compute_scores(labels, preds)
-        assert_eq(expected, got)
+        assert expected == got
 
 
 class TestSmape:
@@ -144,4 +126,4 @@ class TestSmape:
 def test_metrics_have_unique_name():
     metric_by_name = metrics.get_instantiated_metric_dict()
     metric_names = metric_by_name.keys()
-    assert_eq(len(metric_names), len(set(metric_names)))
+    assert len(metric_names) == len(set(metric_names))
