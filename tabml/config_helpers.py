13e9f8351f2f0dbd5a052ec3b6ed9e953a56be49
Tiep Vu <vuhuutiep@gmail.com>
2022-10-28 11:08:58 -0700

    allow parse_pipeline_config/parse_feature_config receive yaml_str as input

--- tabml/config_helpers.py
+++ tabml/config_helpers.py
@@ -6,16 +6,25 @@ import yaml
 from tabml.schemas import feature_config, pipeline_config
 
 
-def parse_feature_config(yaml_path: Union[str, Path]):
-    with open(yaml_path) as f:
-        config = yaml.safe_load(f)
-        return feature_config.FeatureConfig(**config)
+def _parse_yaml_as_config(config_cls, yaml_path: Union[str, Path], yaml_str: str = ""):
+    if yaml_path and yaml_str:
+        raise IOError("Only one of (yaml_path, yam_str) is allowed to be not None.")
+
+    if yaml_path:
+        with open(yaml_path) as f:
+            config = yaml.safe_load(f)
+    else:  # yaml_str is not None
+        config = yaml.load(yaml_str)
+
+    return config_cls(**config)
 
 
-def parse_pipeline_config(yaml_path: Union[str, Path]):
-    with open(yaml_path) as f:
-        config = yaml.safe_load(f)
-        return pipeline_config.PipelineConfig(**config)
+def parse_feature_config(yaml_path: Union[str, Path], yaml_str: str = ""):
+    return _parse_yaml_as_config(feature_config.FeatureConfig, yaml_path, yaml_str)
+
+
+def parse_pipeline_config(yaml_path: Union[str, Path] = "", yaml_str: str = ""):
+    return _parse_yaml_as_config(pipeline_config.PipelineConfig, yaml_path, yaml_str)
 
 
 def save_yaml_config_to_file(
