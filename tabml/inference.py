28b9eae6a612f7cc86a13c2c0a9033de7b678719
Tiep Vu <vuhuutiep@gmail.com>
2022-10-30 23:50:38 -0700

    update inference.py

--- tabml/inference.py
+++ tabml/inference.py
@@ -7,6 +7,7 @@ from typing import Any, Dict, Iterable, List, Union
 from tabml.feature_manager import BaseFeatureManager
 from tabml.model_wrappers import initialize_model_wrapper_from_full_pipeline_pickle
 from tabml.utils.utils import load_pickle
+from tabml.schemas.full_pipeline_data import FullPipelineData
 
 
 @dataclass
@@ -16,11 +17,11 @@ class ModelInference:
 
     def __post_init__(self):
         # Load pickle
-        data = load_pickle(self.full_pipeline_path)
+        data: FullPipelineData = load_pickle(self.full_pipeline_path)
         # Initialize feature_manager
         self.fm = self.feature_manager_cls.from_full_pipeline_data(data)
         # Initialize model_wrapper
-        pipeline_config = data["pipeline_config"]
+        pipeline_config = data.pipeline_config
         self.model_wrapper = initialize_model_wrapper_from_full_pipeline_pickle(data)
         self.features_to_model = list(pipeline_config.data_loader.features_to_model)
 
