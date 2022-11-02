0df6ef4f9e2be300c87c142b6f0cff1ca2dee588
Tiep Vu <vuhuutiep@gmail.com>
2022-11-02 00:18:42 -0700

    pipelines: remove BasePipeline.from_config_path, merge it with __init__

--- tabml/examples/housing/pipelines.py
+++ tabml/examples/housing/pipelines.py
@@ -3,19 +3,19 @@ from tabml.pipelines import BasePipeline
 
 def train_lgbm():
     path_to_config = "configs/lgbm_config.yaml"
-    pipeline = BasePipeline.from_config_path(path_to_config)
+    pipeline = BasePipeline(path_to_config)
     pipeline.run()
 
 
 def train_xgboost():
     path_to_config = "configs/xgboost_config.yaml"
-    pipeline = BasePipeline.from_config_path(path_to_config)
+    pipeline = BasePipeline(path_to_config)
     pipeline.run()
 
 
 def train_catboost():
     path_to_config = "configs/catboost_config.yaml"
-    pipeline = BasePipeline.from_config_path(path_to_config)
+    pipeline = BasePipeline(path_to_config)
     pipeline.run()
 
 
