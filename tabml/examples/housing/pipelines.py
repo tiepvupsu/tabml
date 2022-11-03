c8fe62ce7248ced46745e6a0532b865589f0a7f7
Tiep Vu <vuhuutiep@gmail.com>
2022-11-02 21:31:54 -0700

    housing: merge all tests into one to test prediction features

--- tabml/examples/housing/pipelines.py
+++ tabml/examples/housing/pipelines.py
@@ -5,18 +5,21 @@ def train_lgbm():
     path_to_config = "configs/lgbm_config.yaml"
     pipeline = BasePipeline(path_to_config)
     pipeline.run()
+    return pipeline
 
 
 def train_xgboost():
     path_to_config = "configs/xgboost_config.yaml"
     pipeline = BasePipeline(path_to_config)
     pipeline.run()
+    return pipeline
 
 
 def train_catboost():
     path_to_config = "configs/catboost_config.yaml"
     pipeline = BasePipeline(path_to_config)
     pipeline.run()
+    return pipeline
 
 
 if __name__ == "__main__":
