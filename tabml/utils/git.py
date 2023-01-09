ee975efd4e6414c25858aff2c6496596900b36cb
Tiep Vu <vuhuutiep@gmail.com>
2023-01-09 10:11:23 -0800

    a minor styling

--- tabml/utils/git.py
+++ tabml/utils/git.py
@@ -2,6 +2,6 @@ import git
 
 
 def get_git_repo_dir():
-    """Gets path to the current git repo.  """
+    """Gets path to the current git repo."""
     repo = git.Repo(".", search_parent_directories=True)
     return repo.working_tree_dir
